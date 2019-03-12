cfef29a3bba2a0f7a6cfca833771f7aa9ed0450e
tjallen27 <tjallen91@hotmail.co.uk>
2019-03-12 21:27:41 +0000

    site update

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,15 +6,8 @@ export default class Header extends Component {
       <div className="header">
         <h1 className="header-title">Thomas Allen</h1>
         <div className="header-sub">Full Stack Engineer</div>
-<<<<<<< HEAD
-        <div className="header-current">Currently @Yorlet</div>
-        <div className="header-current">
-          Previous: Junior Web Developer - WCRS
-        </div>
-=======
         <div className="header-current">Currently @yorlet</div>
         <div className="header-current">Previous: WCRS - Junior Developer</div>
->>>>>>> 16795bdd6866b54e32400898092ebcce57a33c01
       </div>
     );
   }
